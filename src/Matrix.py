86e8fca9c2484cf317407e17f02020b9930762c8
sernics <alu0101389936@ull.edu.es>
2022-10-29 11:20:36 +0000

    Arreglado el problema de pintar la matriz

--- src/Matrix.py
+++ src/Matrix.py
@@ -1,28 +1,30 @@
 import tkinter as tk
 
+
 class Matrix(object):
-  def __init__(self, width, height, range_i, range_j):
-    self.__v_width = width
-    self.__v_height = height
-    self.__range_i = range_i
-    self.__range_j = range_j
-    self.__valor_i = self.__v_width / self.__range_i
-    self.__valor_j = self.__v_height / self.__range_j
-    self.__window = tk.Tk()
-    self.__window.title("Matrix")
-    self.__canvas = tk.Canvas(self.__window, width=self.__v_width,
-                              height=self.__v_height, bg="black")
-    self.__canvas.pack()
+    def __init__(self, width, height, range_i, range_j):
+        self.__v_width = width
+        self.__v_height = height
+        self.__range_i = range_i
+        self.__range_j = range_j
+        self.__valor_i = self.__v_width / self.__range_i
+        self.__valor_j = self.__v_height / self.__range_j
+        self.__window = tk.Tk()
+        self.__window.title("Matrix")
+        self.__canvas = tk.Canvas(self.__window, width=self.__v_width,
+                                  height=self.__v_height, bg="black")
+        self.__canvas.pack()
 
-  def print(self):
-    for i in range(self.__range_i):
-      for j in range(self.__range_j):
-        # Create rectangles
-        self.__canvas.create_rectangle(i * self.__valor_i, j * self.__valor_j, i * self.__valor_i +
-                                       self.__valor_i, j * self.__valor_j + self.__valor_j, fill="white")
+    def print(self):
+        for i in range(self.__range_i):
+            for j in range(self.__range_j):
+                # Create rectangles
+                self.__canvas.create_rectangle(i * self.__valor_i, j * self.__valor_j, i *
+                                               self.__valor_i + self.__valor_i, j * self.__valor_j + self.__valor_j, fill="white")
 
-    self.__window.mainloop()
+    def print_position(self, x, y, color):
+        self.__canvas.create_rectangle((x - 1) * self.__valor_i, (y - 1) * self.__valor_j, (x - 1) *
+                                       self.__valor_i + self.__valor_i, (y - 1) * self.__valor_j + self.__valor_j, fill=color)
 
-  # def print_position(self, x, y, color):
-  #   self.__canvas.create_rectangle(x * self.__valor_i, y * self.__valor_j, x *
-  #                                  self.__valor_i + self.__valor_i, y * self.__valor_j + self.__valor_j, fill=color)
+    def execute(self):
+        self.__window.mainloop()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from Matrix import Matrix
 
 def main():
-  matrix = Matrix(1920, 1080, 50, 50)
+  matrix = Matrix(2560, 1080, 50, 50)
   matrix.print()
-  # matrix.print_position(1, 1, "red")
+  matrix.print_position(1, 1, "red")
+  matrix.print_position(2, 1, "orange")
+  matrix.print_position(3, 1, "blue")
+  matrix.print_position(4, 1, "grey")
+  matrix.execute()
   
 if __name__ == "__main__":
   main()
