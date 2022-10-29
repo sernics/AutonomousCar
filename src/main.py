8d254e61bb570489255d9bdd6d01ba49e27900fc
sernics <alu0101389936@ull.edu.es>
2022-10-29 10:15:26 +0000

    Hecha la tabla con valores automatizados, ahora falta hacer la seleccion por usuario y meterlo en una clase

--- src/main.py
+++ src/main.py
@@ -4,20 +4,29 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Matrix")
 
+v_width = 2560
+v_height = 1080
+
 # Create a canvas
-canvas = tk.Canvas(window, width=500, height=500, bg="black")
+canvas = tk.Canvas(window, width = v_width, height = v_height, bg="black")
 canvas.pack()
 
+range_i = 50
+range_j = 50
+
+valor_i = v_width / range_i
+valor_j = v_height / range_j
+
 # Print the matri(x - 1)
-for i in range(50):
-  for j in range(50):
+for i in range(range_i):
+  for j in range(range_j):
     # Create rectangles
-    canvas.create_rectangle(i*10, j*10, i*10+10, j*10+10, fill="white")
+    canvas.create_rectangle(i * valor_i, j * valor_j, i * valor_i + valor_i, j * valor_j + valor_j, fill="white")
 
 # Print a car in a position
 def print_car(x, y, color):
-  # Create a rectangle
-  canvas.create_rectangle(x*10, y*10, x*10+10, y*10+10, fill=color)
+  # Print the rectangle
+  canvas.create_rectangle(x * valor_i, y * valor_j, x * valor_i + valor_i, y * valor_j + valor_j, fill=color)
 
 print_car(10, 10, "red")
 
